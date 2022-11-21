86a938d94286f013bf10f7a3b632a5e570b96519
Nicolas Delauney <nicolas.delauney@sinch.com>
2022-11-21 17:44:10 +0100

    ✨ Basic run and rendering

--- src/domain.go
+++ src/domain.go
@@ -1,5 +1,13 @@
 package preflight
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/lipgloss"
+)
+
 type Checkpoint struct {
 	Name          string `yaml:"name"`
 	Command       string `yaml:"command"`
@@ -13,4 +21,40 @@ type SystemCheck struct {
 	Optional    bool         `yaml:"optional"`
 	Checkpoints []Checkpoint `yaml:"options"`
 	Check       bool
+}
+
+var (
+	currentPkgNameStyle = lipgloss.NewStyle().Foreground(honey)
+	pkgNameStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("242"))
+	checkMark           = lipgloss.NewStyle().Foreground(lipgloss.Color("42")).SetString("✓")
+	koMark              = lipgloss.NewStyle().Foreground(lipgloss.Color("160")).SetString("✕")
+)
+
+func (s SystemCheck) Render(active bool, spinner spinner.Model) string {
+	icon := pkgNameStyle.Render("-")
+	checkName := pkgNameStyle.Render(s.Name)
+
+	if active {
+		icon = spinner.View()
+		checkName = currentPkgNameStyle.Render(s.Name)
+	}
+
+	return fmt.Sprintf("%s %s\n", icon, checkName)
+}
+
+func (s SystemCheck) RenderResult() string {
+	icon := checkMark.String()
+	name := pkgNameStyle.Render(s.Name)
+	desc := strings.Builder{}
+
+	if !s.Check {
+		icon = koMark.String()
+		name = koMark.Render(s.Name)
+		desc.WriteString(fmt.Sprintf("\n\t%s", s.Description))
+		for _, checkpoint := range s.Checkpoints {
+			desc.WriteString(fmt.Sprintf("\n\t%s\t%s", checkpoint.Name, checkpoint.Documentation))
+		}
+	}
+
+	return fmt.Sprintf("%s %s%s\n", icon, name, pkgNameStyle.Render(desc.String()))
 }
--- src/runner.go
+++ src/runner.go
@@ -3,6 +3,7 @@ package preflight
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/spinner"
@@ -11,12 +12,8 @@ import (
 )
 
 var (
-	honey               = lipgloss.Color("#febe3c")
-	ocean               = lipgloss.Color("#1686cb")
-	pkgNameStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("242"))
-	currentPkgNameStyle = lipgloss.NewStyle().Foreground(honey)
-	checkMark           = lipgloss.NewStyle().Foreground(lipgloss.Color("42")).SetString("✓")
-	koMark              = lipgloss.NewStyle().Foreground(lipgloss.Color("160")).SetString("✕")
+	honey = lipgloss.Color("#febe3c")
+	ocean = lipgloss.Color("#1686cb")
 )
 
 type preflightModel struct {
@@ -46,6 +43,7 @@ func PreflighModel(systemCheck []SystemCheck) preflightModel {
 
 func (p preflightModel) Init() tea.Cmd {
 	return tea.Batch(
+		p.checks[p.activeIndex].run(),
 		p.spinner.Tick,
 	)
 }
@@ -58,6 +56,21 @@ func (p preflightModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "esc", "q":
 			return p, tea.Quit
 		}
+	case systemCheckMsg:
+		if p.activeIndex >= len(p.checks)-1 {
+			// Everything's been installed. We're done!
+			p.done = true
+			return p, tea.Quit
+		}
+		p.checks[p.activeIndex].Check = msg.check
+		p.activeIndex++
+		// Update progress bar
+		progressCmd := p.progress.SetPercent(float64(p.activeIndex) / float64(len(p.checks)))
+		return p, tea.Batch(
+			progressCmd,
+			tea.Printf(p.checks[p.activeIndex-1].RenderResult()),
+			p.checks[p.activeIndex].run(),
+		)
 	case progress.FrameMsg:
 		newModel, cmd := p.progress.Update(msg)
 		if newModel, ok := newModel.(progress.Model); ok {
@@ -87,4 +100,16 @@ func (p preflightModel) View() string {
 	view.WriteString(fmt.Sprintf(" %d / %d", p.activeIndex+1, len(p.checks)))
 
 	return view.String()
+}
+
+type systemCheckMsg struct{ check bool }
+
+func (s SystemCheck) run() tea.Cmd {
+	for _, checkpoint := range s.Checkpoints {
+		tea.Println(checkpoint.Name)
+	}
+	d := time.Millisecond * time.Duration(500)
+	return tea.Tick(d, func(t time.Time) tea.Msg {
+		return systemCheckMsg{check: false}
+	})
 }
