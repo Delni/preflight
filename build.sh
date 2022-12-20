c7a95f7b8a06156e52c55d328af0c6ee412bab46
ThibaudMZN <thibaud.marazano@gmail.com>
2022-12-20 14:34:20 +0100

    🐛 Adding ext for windows build

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 OUT="bin/"
 TARGETS_OS="darwin freebsd linux openbsd windows"
 TARGETS_ARCH="amd64 arm64"
+EXT=""
 
 for os in $TARGETS_OS; do
     for arch in $TARGETS_ARCH; do
         echo "Building $arch on $os"
-        GOOS=$os GOARCH=$arch go build -o "${OUT}preflight_${os}_${arch}"
+        ext=$([[ "$os" == "windows" ]] && echo ".exe" || echo "")
+        GOOS=$os GOARCH=$arch go build -o "${OUT}preflight_${os}_${arch}${ext}"
     done
 done
