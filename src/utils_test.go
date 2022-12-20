ba93b22e83573074b14341ded6b32ff90a96795e
ThibaudMZN <thibaud.marazano@gmail.com>
2022-12-20 14:57:39 +0100

    ✅ First test on Utils

---
+++ src/utils_test.go
@@ -0,0 +1,45 @@
+package preflight
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetInterpreterCommand(t *testing.T) {
+	var tests = []struct {
+		os   string
+		want OSInterpreter
+	}{
+		{"windows", OSInterpreter{
+			Interpreter:                "powershell.exe",
+			InterpreterArgs:            "",
+			InterpreterInteractiveArgs: "",
+			Command:                    "command",
+			CommandArgs:                "",
+		}},
+		{"darwin", OSInterpreter{
+			Interpreter:                "bash",
+			InterpreterArgs:            "-c",
+			InterpreterInteractiveArgs: "-ic",
+			Command:                    "command",
+			CommandArgs:                "-v",
+		}},
+		{"linux", OSInterpreter{
+			Interpreter:                "bash",
+			InterpreterArgs:            "-c",
+			InterpreterInteractiveArgs: "-ic",
+			Command:                    "command",
+			CommandArgs:                "-v",
+		}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("GetIntrepreterCommand for OS %s", tt.os)
+		t.Run(testname, func(t *testing.T) {
+			ans := getInterpreterCommand(tt.os)
+			if ans != tt.want {
+				t.Errorf("got %+v, want %+v", ans, tt.want)
+			}
+		})
+	}
+}
