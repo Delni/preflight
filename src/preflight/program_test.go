36caec51ff5bf4624a3d5957ff4e5fc988b3a542
Delni <niko-d@live.fr>
2022-12-29 12:20:26 +0100

    ✅ Add preflight program tests

--- src/preflight/program_test.go
+++ src/preflight/program_test.go
@@ -3,6 +3,9 @@ package preflight
 import (
 	"strings"
 	"testing"
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func assertMultipleContains(t *testing.T, ans string, want []string) {
@@ -29,4 +32,43 @@ func TestViewDone(t *testing.T) {
 
 	want := []string{"✕", "SYSTEM_CHECK", "DESCRIPTION", "CHECKPOINT", "DOC", "No go, no go! Check above for more details. 🛬"}
 	assertMultipleContains(t, ans, want)
+}
+
+func TestUpdateWithSystemCheckMsgShouldUpdateInternalState(t *testing.T) {
+	p := makeTestModel()
+
+	raw_ans, _ := p.Update(systemCheckMsg{check: true})
+	ans := raw_ans.(PreflightModel)
+	if ans.done != true || ans.activeIndex != 1 {
+		t.Errorf("got %+v", ans)
+	}
+}
+
+func TestUpdateWithFrameMsgShouldUpdateProgress(t *testing.T) {
+	p := makeTestModel()
+
+	raw_ans, _ := p.Update(progress.FrameMsg{})
+	ans := raw_ans.(PreflightModel)
+	if &ans.progress == &p.progress {
+		t.Errorf("got %+v", ans)
+	}
+}
+
+func TestUpdateWithWindowSizeMsgShouldUpdateProgressWidth(t *testing.T) {
+	p := makeTestModel()
+
+	raw_ans, _ := p.Update(tea.WindowSizeMsg{Width: 100})
+	ans := raw_ans.(PreflightModel)
+	if ans.progress.Width != 100 {
+		t.Errorf("got %+v", ans)
+	}
+}
+
+func TestShouldQuitOnCtrlC(t *testing.T) {
+	p := makeTestModel()
+
+	_, cmd := p.Update(tea.KeyMsg{Type: tea.KeyCtrlC})
+	if cmd == nil {
+		t.Errorf("got %+v", cmd)
+	}
 }
