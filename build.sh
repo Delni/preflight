eb347aade0d7821208b036a8feacc5c8066f93ba
Delni <niko-d@live.fr>
2022-12-30 12:07:02 +0100

    🔨 Ease download with zip files

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ for os in $TARGETS_OS; do
     for arch in $TARGETS_ARCH; do
         echo "Building $arch on $os"
         ext=$([[ "$os" == "windows" ]] && echo ".exe" || echo "")
-        GOOS=$os GOARCH=$arch go build -o "${OUT}preflight_${os}_${arch}${ext}"
+        GOOS=$os GOARCH=$arch go build -o "${OUT}preflight${ext}"
+        zip -r "${OUT}preflight_${os}_${arch}.zip" "${OUT}preflight${ext}"
+        rm "${OUT}preflight${ext}"
     done
 done
