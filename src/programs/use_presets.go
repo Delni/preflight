f6523f65ca99b387233702f6b622244635825c79
Delni <niko-d@live.fr>
2023-01-05 16:07:04 +0100

    ✅ Test use_presets
    Co-authored-by: zoedrey <monitorpattern@users.noreply.github.com>

--- src/programs/use_presets.go
+++ src/programs/use_presets.go
@@ -5,14 +5,27 @@ import (
 	p "preflight/presets"
 	"preflight/src/styles"
 	"preflight/src/systemcheck"
+	"sort"
 	"strings"
 )
 
-func UsePresets(presets []string) []systemcheck.SystemCheck {
+func displayPresetError(err_presets []string) {
+	fmt.Println(styles.KoMark.Render("Unknown presets:"))
+	for _, err := range err_presets {
+		fmt.Println(styles.KoMark.Render("   - " + err))
+	}
+
+	fmt.Println(styles.KoMark.Render("\n" + AvailablePresets(p.Presets)))
+	fmt.Println(styles.PkgNameStyle.Render("Thinks that something is missing? Please open an issue on https://github.com/delni/preflight/issues/new"))
+
+	fmt.Println()
+}
+
+func UsePresets(presetsCandidate []string, knownPresets map[string]systemcheck.SystemCheck) []systemcheck.SystemCheck {
 	var err_presets = []string{}
 	var systemcheck = []systemcheck.SystemCheck{}
-	for _, presetName := range presets {
-		preset, exists := p.Presets[presetName]
+	for _, presetName := range presetsCandidate {
+		preset, exists := knownPresets[presetName]
 		if !exists {
 			err_presets = append(err_presets, presetName)
 		} else {
@@ -20,27 +33,22 @@ func UsePresets(presets []string) []systemcheck.SystemCheck {
 		}
 	}
 	if len(err_presets) > 0 {
-		fmt.Println(styles.KoMark.Render("Unknown presets:"))
-		for _, err := range err_presets {
-			fmt.Println(styles.KoMark.Render("   - " + err))
-		}
-
-		fmt.Println(styles.KoMark.Render("\n" + AvailablePresets()))
-		fmt.Println(styles.PkgNameStyle.Render("Thinks that something is missing? Please open an issue on https://github.com/delni/preflight/issues/new"))
-
-		fmt.Println()
+		displayPresetError(err_presets)
 	}
 	return systemcheck
 }
 
-func AvailablePresets() string {
+func AvailablePresets(knownPresets map[string]systemcheck.SystemCheck) string {
 	var (
 		builder = strings.Builder{}
 		keys    = []string{}
 	)
-	for name := range p.Presets {
+
+	for name := range knownPresets {
 		keys = append(keys, name)
 	}
+
+	sort.Strings(keys)
 	builder.WriteString("Available presets are: ")
 	builder.WriteString(strings.Join(keys, ", "))
 	return builder.String()
