599914808a925d4ce5a0e75ffd3b66d958294298
Delni <niko-d@live.fr>
2023-01-05 11:57:19 +0100

    ✨Add node versions managers

--- presets/main.go
+++ presets/main.go
@@ -4,5 +4,6 @@ import "preflight/src/systemcheck"
 
 // Please keep this map in alphabetical order
 var Presets = map[string]systemcheck.SystemCheck{
-	"node": node,
+	"node":         node,
+	"node-version": nodeVersion,
 }
